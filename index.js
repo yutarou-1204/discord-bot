9458a00e171539d8cc65e8ba8b66be64f79a9099
yutarou-1204 <61963527+yutarou-1204@users.noreply.github.com>
2025-07-12 18:10:14 +0900

    fix: update SERVER_ID for ConoHa API integration

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const client = new Client({
 const CONOHA_IDENTITY_URL = "https://identity.c3j1.conoha.io/v3/auth/tokens";
 const CONOHA_COMPUTE_URL = "https://compute.c3j1.conoha.io/v2.1";
 const TENANT_ID = "7544f37d10be4ff7a638d1b34c6732b1";
-const SERVER_ID = "b9d544e5-5606-4125-81f8-05a61d1e6f01";
+const SERVER_ID = "	9900f299-4fac-42ce-a590-fcc7f5020922";
 const USERNAME = "gncu33184909";
 const PASSWORD = "Y6xLYEsN-k3muLU";
 
